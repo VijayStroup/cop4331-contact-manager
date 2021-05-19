c98519dd3560cb4fa55357661e3b44862ecabad6
Vijay Stroup <vijaystroup@gmail.com>
2021-05-19 11:03:00 -0400

    home page route

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from routers import api
+from fastapi.staticfiles import StaticFiles
+from routers import api, pages
 
 app = FastAPI()
 app.include_router(api)
+app.include_router(pages)
+app.mount('/static', StaticFiles(directory='static'), name='static')
--- routers/pages.py
+++ routers/pages.py
@@ -1,7 +1,17 @@
+from fastapi import Request
 from fastapi import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter(
     prefix = '',
     tags = ['pages'],
     responses = {404: {'description': 'Not found'}},
 )
+
+templates = Jinja2Templates(directory="templates")
+
+
+@router.get('/', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse('home.html', {'request': request})
