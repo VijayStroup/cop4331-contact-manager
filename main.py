67a0be55706eca3163af57471ff92c67a1fd2199
Vijay Stroup <vijaystroup@gmail.com>
2021-05-19 11:09:44 -0400

    favicon

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi.staticfiles import StaticFiles
 from routers import api, pages
 
 app = FastAPI()
-app.include_router(api)
-app.include_router(pages)
+app.include_router(api.router)
+app.include_router(pages.router)
 app.mount('/static', StaticFiles(directory='static'), name='static')
