a06315a83297a73ddeb09d2acd54723fc71921e9
Vijay Stroup <vijaystroup@gmail.com>
2021-05-19 11:22:19 -0400

    some db methods

--- db.py
+++ db.py
@@ -9,3 +9,23 @@ class DB:
         """initalize database and tables if not created"""
 
         pass
+
+    def new_user(self, name: str, password: str):
+        """add a new user to the database"""
+
+        pass
+
+    def new_contact(self, id: str, name: str):
+        """create a new contact for the user"""
+
+        pass
+
+    def del_contact(self, id: str, name: str):
+        """delete contact for the user"""
+
+        pass
+
+    def get_contacts(self, id: str):
+        """return a list of contacts for the user"""
+
+        pass
