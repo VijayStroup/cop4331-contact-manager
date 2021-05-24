69f584aacdf0e0517e6c772aa6d9f4eee269088c
JayHenry07 <sevenjrh@brighthouse.com>
2021-05-24 11:37:55 -0400

    Added methods: get_all_contacts and delete_contact

--- routers/api.py
+++ routers/api.py
@@ -16,3 +16,21 @@ def new_user(name: str, password: str):
     db.db.execute('insert into users values (?, ?)', (name, password,))
 
     return {'user': user}
+
+@router.get('/contact')
+def get_all_contacts(id: str):
+    contacts, error, message = db.get_contacts(id)
+
+    if not error:
+        return {'contacts': contacts, 'error': message}
+    else:
+        pass
+
+@router.delete('/contact')
+def delete_contact(id: str, name: str):
+    error, message = db.del_contact(id, name)
+
+    if not error:
+        return {'error': message}
+    else:
+        pass
