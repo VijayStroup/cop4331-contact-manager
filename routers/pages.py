593c742ed98b8b03d99f349052a5b3b03dece80a
Vijay Stroup <vijaystroup@gmail.com>
2021-05-25 23:15:56 -0400

    contacts html route

--- routers/pages.py
+++ routers/pages.py
@@ -15,3 +15,8 @@ templates = Jinja2Templates(directory="templates")
 @router.get('/', response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
+
+
+@router.get('/contacts', response_class=HTMLResponse)
+def contacts(request: Request):
+    return templates.TemplateResponse('contacts.html', {'request': request})
