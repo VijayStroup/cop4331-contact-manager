e6fb77152d73f13b1475c3aefd553bf9561ace66
Vijay Stroup <vijaystroup@gmail.com>
2021-05-26 11:33:27 -0400

    add pages routes

--- routers/pages.py
+++ routers/pages.py
@@ -17,6 +17,11 @@ def home(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
 
 
+@router.get('/register', response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse('register.html', {'request': request})
+
+
 @router.get('/contacts', response_class=HTMLResponse)
 def contacts(request: Request):
     return templates.TemplateResponse('contacts.html', {'request': request})
