8d96b17029b208d073fec0b4b23f6848166860af
gexrgia <gnewbery22@gmail.com>
2021-06-01 15:17:00 -0400

    login and register functions

--- static/login.js
+++ static/login.js
@@ -1,4 +1,66 @@
 // on login
 document.querySelector('#login-btn').addEventListener('click', () => {
   console.log('login')
+
+  const user_input = document.querySelector('#username').value;
+  const pass_input = document.querySelector('#password').value;
+
+  const jwt = getCookie('token');
+  
+  fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${jwt}`
+    },
+    body: JSON.stringify({
+      username: user_input, 
+      password: pass_input
+    })
+  })
+  
+  .then(res => res.json)
+  .then(data => console.log(data['token']))
+  .then(res => {
+    if (res.ok) {
+      setCookie('token', data['token'], 30); 
+      console.log('Success')
+    } else {
+      console.log('Incorrect username/password')
+    }
+  })
+
+  .catch(error => console.error('Error', error))
 })
+
+export function setCookie(cname, cvalue, exdays) {
+  if (typeof window != 'undefined') {
+    let d = new Date()
+    d.setTime(d.getTime() + (exdays*24*60*60*1000))
+    const expires = 'expires='+ d.toUTCString()
+
+    if (process.env.NEXT_PUBLIC_STATUS == 'dev')
+      document.cookie = `${cname}=${cvalue};${expires};samesite=strict;path=/`
+    else
+      document.cookie = `${cname}=${cvalue};${expires};secure;samesite=strict;path=/`
+  }
+}
+
+export function getCookie(cname) {
+  if (typeof window != 'undefined') {
+    let name = cname + '='
+    let decodedCookie = decodeURIComponent(document.cookie)
+    let ca = decodedCookie.split(';')
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i]
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1)
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length)
+      }
+    }
+  }
+  return null
+}
